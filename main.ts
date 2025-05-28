6dcd2e742ca558d69a46c51867f30090ec082e9f
Frederik Martini <fma@cvation.com>
2025-05-28 20:01:52 +0200

    upload

--- main.ts
+++ main.ts
@@ -1,134 +1,93 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, FileSystemAdapter, Modal, Notice, Plugin, Setting } from 'obsidian';
+import { exec } from 'child_process';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+export default class HelloWorldPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.addRibbonIcon('arrow-down-to-line', 'Pull', (evt: MouseEvent) => {
+			new Notice("Initiating pull")
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+			const vaultPath = this.getVaultAbsolutePath()
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+			if (vaultPath == null) {
+				new Notice("Failed to get vault directory");
+				return
 			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+			exec('git pull', {cwd: vaultPath}, (error, stdout, _) => {
+				if (error) {
+					new Notice(error.message)
+					return;
 				}
+				new Notice(stdout);
+			});
+		});
+
+		this.addRibbonIcon('arrow-up-from-line', 'Push', (evt: MouseEvent) => {
+			const vaultPath = this.getVaultAbsolutePath()
+
+			if (vaultPath == null) {
+				new Notice("Failed to get vault directory");
+				return
 			}
+
+			new PushModal(this.app, (commitMessage) => {
+				new Notice(`Initiating push with message '${commitMessage}'`)
+
+				exec('git add *', {cwd: vaultPath})
+				exec(`git commit -m "${commitMessage}"`, {cwd: vaultPath})
+				exec('git push', {cwd: vaultPath}, (error, stdout, _) => {
+					if (error) {
+						new Notice(error.message)
+						return;
+					}
+					new Notice(`Pushed successful`);
+				});
+			}).open();
 		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
-
 	}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
 	}
 
 	async saveSettings() {
-		await this.saveData(this.settings);
+	}
+
+	getVaultAbsolutePath(): string | null {
+		const adapter = this.app.vault.adapter;
+		if (adapter instanceof FileSystemAdapter) {
+			return adapter.getBasePath();
+		}
+
+		return null
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+export class PushModal extends Modal {
+  constructor(app: App, onSubmit: (result: string) => void) {
+    super(app);
+	this.setTitle('Enter commit message');
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+	let message = '';
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+    new Setting(this.contentEl)
+      .addText((text) =>
+        text.onChange((value) => {
+          message = value;
+        }));
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    new Setting(this.contentEl)
+      .addButton((btn) =>
+        btn
+          .setButtonText('Push')
+          .setCta()
+          .onClick(() => {
+            this.close();
+            onSubmit(message);
+          }));
+  }
 }
